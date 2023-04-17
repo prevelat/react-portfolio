06bb639cb9e9207bd6f8cd9462fdb04d21b4c0ee
prevelat <prevelat3@gmail.com>
2023-04-17 18:41:36 -0300

    v1.0 -fix

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import NavBar from './components/NavBar'
 import Home from './components/Home'
 import SocialLinks from './components/SocialLinks'
 import About from './components/About'
-import Portfolio from './components/Portfolio'
+// import Portfolio from './components/Portfolio'
 import Experience from './components/Experience'
 import Contact from './components/Contact'
 
